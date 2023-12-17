0cbe2fda37265aa85c2311c0764f50ffd92b5264
avalmly <146702530+avalmly@users.noreply.github.com>
2023-12-17 15:05:48 +0100

    Update nuevo_recurso.py

--- nuevo_recurso.py
+++ nuevo_recurso.py
@@ -34,7 +34,7 @@ def crear_grupo_ad(host, usuario, contrasena, grupo):
             raise Exception("Error al copiar la clave pública al servidor.")
 
         # Comando de ejecución de PowerShell
-        cmd_ssh = f'sshpass -p "{contrasena}" ssh {usuario}@{host} "powershell -NoProfile -NonInteractive -Command "New-ADGroup -Name {grupo} -GroupScope Global""'
+        cmd_ssh = f'sshpass -p "{contrasena}" ssh {usuario}@{host} "powershell -NoProfile -NonInteractive -Command "New-ADGroup -Name {grupo} -GroupScope Global""'        
         
         # Ejecutar el comando
         subprocess.run(cmd_ssh, shell=True)
@@ -49,6 +49,7 @@ def check_grupo(host, usuario, contrasena, grupo):
         if grupo not in lista_grupos.stdout:
             print("El grupo especificado no existe. Creando grupo...")
             crear_grupo_ad(host, usuario, contrasena, grupo)
+            os.system(f"systemctl restart winbind") # Para que aparezcan los nuevos grupos al hacer un getent
         else:
             print("El grupo ya existe")
     except subprocess.CalledProcessError:
@@ -58,7 +59,7 @@ def crea_recurso(recurso, ruta, grupo):
     try:
         with open("plantilla_recurso", 'r') as plantilla_recurso:
             lineas = plantilla_recurso.read()
-            
+
         # Realizar las sustituciones
         lineas_modificadas = (
             lineas
@@ -66,23 +67,31 @@ def crea_recurso(recurso, ruta, grupo):
             .replace('ruta', ruta)
             .replace('grupo', grupo)
         )
-            
-        samba_file = open("/etc/samba/smb.conf", "a")                       # modo append
-        samba_file.write(f'\n{lineas_modificadas}\n')                       # Añade al final del fichero el recurso
-        samba_file.close()
-        
-        # Verificar si el directorio ya existe antes de intentar crearlo
-        if not os.path.exists(f"/recursos/{recurso}"):
-            # Crear el directorio del recurso
-            os.makedirs(f"/recursos/{recurso}")
 
-            # Configurar permisos del directorio
-            os.chmod(f"/recursos/{recurso}", 0o770)
-            os.system(f"chown :{grupo} /recursos/{recurso}")
+        smb_conf_path = "/etc/samba/smb.conf"
 
-            print(f"Recurso '{recurso}' y directorio creado con éxito.")
-        else:
-            print(f"El directorio '/recursos/{recurso}' ya existe.")
+        # Verificar si el recurso ya existe en smb.conf
+        with open(smb_conf_path, 'r') as smb_conf:
+            if f"[{recurso}]" not in smb_conf.read():
+                # Añadir las líneas modificadas al archivo smb.conf
+                with open(smb_conf_path, "a") as samba_file:
+                    samba_file.write(f'\n{lineas_modificadas}\n')
+                    
+                # Verificar si el directorio ya existe antes de intentar crearlo
+                if not os.path.exists(f"/recursos/{recurso}"):
+                    # Crear el directorio del recurso
+                    os.makedirs(f"/recursos/{recurso}")
+
+                    # Configurar permisos del directorio
+                    os.chmod(f"/recursos/{recurso}", 0o770)
+                    os.system(f"chown :{grupo} /recursos/{recurso}")
+
+                    print(f"Recurso '{recurso}' y directorio creado con éxito.")
+                else:
+                    print(f"El directorio '/recursos/{recurso}' ya existe.")
+            else:
+                print(f"El recurso '{recurso}' ya existe en smb.conf.")
+
     except FileNotFoundError:
         print("Error: No se encontró el archivo de plantilla 'plantilla_recurso'.")
     except Exception as e:
