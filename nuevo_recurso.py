0afc21d3471fc6572db430f4b1bcd81ee86e4d41
avalmly <146702530+avalmly@users.noreply.github.com>
2023-12-17 10:02:03 +0100

    Update nuevo_recurso.py

--- nuevo_recurso.py
+++ nuevo_recurso.py
@@ -10,23 +10,31 @@
 import os
 import sys
 import subprocess
-import pywinrm
+import winrm
+
+def check_grupo(grupo):
+    lista_grupos = subprocess.run(['getent', 'group'], capture_output=True, text=True)
+    if (grupo not in lista_grupos):
+        print("El grupo especificado no existe. Creando grupo...")
+
+def crea_recurso(recurso, ruta, grupo):
+    with open("plantilla_recurso", 'r') as plantilla_recurso:
+        lineas = plantilla_recurso.read()
+            
+    # Realizar las sustituciones
+    lineas_modificadas = (
+        lineas
+        .replace('recurso', recurso)
+        .replace('ruta', ruta)
+        .replace('grupo', grupo)
+    )
+            
+    samba_file = open("/etc/samba/smb.conf", "a")             # modo append
+    samba_file.write(f'\n{lineas_modificadas}\n')             # Añade al final del fichero el recurso
+    samba_file.close()
 
 recurso = sys.argv[1]
 grupo = sys.argv[2]
 ruta = f"/recursos/{recurso}"
 
-with open("plantilla_recurso", 'r') as plantilla_recurso:
-    lineas = plantilla_recurso.readlines()
-            
-# Realizar las sustituciones
-lineas_modificadas = (
-    lineas
-    .replace('recurso', recurso)
-    .replace('ruta', ruta)
-    .replace('grupo', grupo)
-)
-            
-samba_file = open("/etc/smb.conf", "a")             # modo append
-samba_file.write(f'\n{lineas_modificadas}\n')       # Añade al final del fichero el recurso
-samba_file.close()
+check_grupo(grupo)
