be33d882857701874981b319d7be299ac592ed09
avalmly <146702530+avalmly@users.noreply.github.com>
2023-12-17 10:15:02 +0100

    Update nuevo_recurso.py

--- nuevo_recurso.py
+++ nuevo_recurso.py
@@ -12,10 +12,27 @@ import sys
 import subprocess
 import winrm
 
-def check_grupo(grupo):
+def check_grupo(host, usuario, contrasena, grupo):
     lista_grupos = subprocess.run(['getent', 'group'], capture_output=True, text=True)
     if (grupo not in lista_grupos):
         print("El grupo especificado no existe. Creando grupo...")
+        crear_grupo_ad(host, usuario, contrasena, grupo)
+        
+def crear_grupo_ad(host, usuario, contrasena, grupo):
+    sesion = winrm.Session(
+        host, 
+        auth=(usuario, contrasena),
+        server_cert_validation='ignore'                                 # Ignora la validación del certificado en entornos de prueba
+    )
+
+    # Comando PowerShell para crear un grupo en Active Directory
+    script_ps = f'New-ADGroup -Name "{grupo}" -GroupScope Global'       # Al no especificar GroupSecurity, se crea uno de Seguridad por defecto
+
+    # Ejecuta el script
+    resultado = sesion.run_ps(script_ps)
+
+    # Imprime el resultado
+    print(resultado.std_out.decode('utf-8'))
 
 def crea_recurso(recurso, ruta, grupo):
     with open("plantilla_recurso", 'r') as plantilla_recurso:
@@ -29,12 +46,16 @@ def crea_recurso(recurso, ruta, grupo):
         .replace('grupo', grupo)
     )
             
-    samba_file = open("/etc/samba/smb.conf", "a")             # modo append
-    samba_file.write(f'\n{lineas_modificadas}\n')             # Añade al final del fichero el recurso
+    samba_file = open("/etc/samba/smb.conf", "a")                       # modo append
+    samba_file.write(f'\n{lineas_modificadas}\n')                       # Añade al final del fichero el recurso
     samba_file.close()
+
 
 recurso = sys.argv[1]
 grupo = sys.argv[2]
 ruta = f"/recursos/{recurso}"
+host = "serverad"
+usuario = "administrador"
+password = "Departamento1!"
 
-check_grupo(grupo)
+check_grupo(host, usuario, password, grupo)
